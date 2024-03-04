61df81ecda38e8c0fbeb1ec11621870b3038107d
RajkumarGapat <rajkumargapat345@gmail.com>
2024-03-04 23:12:45 +0530

    PM17

---
+++ Day 1 PM17/demo4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("\n \\a =%d", '\a'); //7
+    printf("\n \\b =%d", '\b'); //8
+    printf("\n \\t =%d", '\t'); //9
+    printf("\n \\n =%d", '\n'); //10
+    printf("\n \\r =%d", '\r'); //13
+    return 0;
+}
